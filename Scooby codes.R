a0a6076bbafee10f880f3d9d4d8bf80479f722d2
epistreams <epistreams@outlook.com>
2021-12-23 14:55:03 -0500

    Set the groundwork for the gender analysis. I am having some trouble dealing with the NULL genders in culprit_gender.

--- Scooby codes.R
+++ Scooby codes.R
@@ -1,12 +1,15 @@
 
+# Installing packages -----------------------------------------------------
+install.packages("gtsummary")
+
 # Loading packages --------------------------------------------------------
 library(tidyverse)
-
+library(gtsummary)
 
 
 
 # Raw data ----------------------------------------------------------------
-scooby_raw <- read_rds("C:/Users/colvic01/Downloads/scoobydoo.rds")
+scooby_raw <- read_rds("base_scoob.RDS")
 scooby_t <- tibble(scooby_raw)
 
 
@@ -20,7 +23,8 @@ scooby_t <- tibble(scooby_raw)
 scooby_nocameo <- scooby_t %>%
  #lower casing the series names to make them easier to work with
   mutate(
-    series_name = tolower(series_name)) %>%
+    series_name = tolower(series_name),
+    year_aired = substr(date_aired, 1, 4)) %>%
   #filtering the series names to retain only those with the words "scooby",
   #"doo", "warner", or "hanna" in them. These titles were chosen by visual inspection.
   #Note: I excluded the arabian nights "TV Special" b/c it lacks a traditional scooby plot.
@@ -41,3 +45,27 @@ scooby_tv_full <- filter(scooby_tv, !grepl("segmented", format))
 scooby_tv_segment <- filter(scooby_tv, grepl("segmented", format))
 scooby_movie_tv <- filter(scooby_movie, !grepl("Theatrical", format))
 scooby_movie_thr <- filter(scooby_movie, grepl("Theatrical", format))
+
+
+
+
+
+# TV analyses -------------------------------------------------------------
+
+scooby_tv_gender <- scooby_tv %>% 
+  mutate(
+    decade = case_when(
+    grepl("196|197|198", year_aired) == TRUE ~ "1960s-1980s",
+    grepl("199|200|201|202", year_aired) == TRUE ~ "1990s-2020s",
+    TRUE ~ as.character(decade)),
+    female_culprit = case_when(
+      grepl("Female", culprit_gender) == TRUE ~ "yes",
+      grepl("Female|NULL", culprit_gender) == FALSE ~ "no",
+      grepl("NULL", culprit_gender) == TRUE ~ "NULL"
+      )
+  )
+tbl_summary(
+  data = scooby_tv_gender,
+  by = decade,
+  include = c("female_culprit")
+)
